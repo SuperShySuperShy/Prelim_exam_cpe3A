091d94fa26800dcf40bd22fef7d902b2e333926d
SuperShySuperShy <alsonnguanlao28@gmail.com>
2023-08-24 16:45:23 +0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,12 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I am Alsonn Joseph C. Guanlao a 3rd Year Computer Engineering Student</p>
+        <p>What is your impression on your first month as an 3rd Year Computer Engineering Student?</p>
+        <p>My impression on my first month as an 3rd Year Computer Engineering Student is that I will learn many more things unlike last year.
+          I'm excitedly looking forward to this year because We are rewarded with Professors and Instructors who are passionate in their jobs and in teaching.
+          I also think that I will learn many things in Software Development and other electronics subjects. I am also looking forward to the new officers of 
+          the organization of Computer Engineering and on what they would do to keep the program lively and more welcoming unlike before.
+        </p>
       </section>
     </Layout>
   );
