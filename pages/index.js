494496abdd2e0f871aeee13e49499ce82c1625a9
SuperShySuperShy <alsonnguanlao28@gmail.com>
2023-08-24 16:53:21 +0800

    third commit

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
           I also think that I will learn many things in Software Development and other electronics subjects. I am also looking forward to the new officers of 
           the organization of Computer Engineering and on what they would do to keep the program lively and more welcoming unlike before.
         </p>
+        <p>I know now how to update my website.</p>
+        <p>We are trying to build a sample website using: <a href = "https://nextjs.org/learn"> our Next.js tutorial</a></p>
+        <p></p>
       </section>
     </Layout>
   );
