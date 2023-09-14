4c3d67bb4188c4d448f967a284bee3fc7f73b47b
SuperShySuperShy <alsonnguanlao28@gmail.com>
2023-09-14 16:13:54 +0800

    prelims commit

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,22 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>What is your impression on your first month as an 3rd Year Computer Engineering Student?</p>
-        <p>My impression on my first month as an 3rd Year Computer Engineering Student is that I will learn many more things unlike last year.
-          I'm excitedly looking forward to this year because We are rewarded with Professors and Instructors who are passionate in their jobs and in teaching.
-          I also think that I will learn many things in Software Development and other electronics subjects. I am also looking forward to the new officers of 
-          the organization of Computer Engineering and on what they would do to keep the program lively and more welcoming unlike before.
-        </p>
-        <p>I know now how to update my website.</p>
-        <p>We are trying to build a sample website using: <a href = "https://nextjs.org/learn"> our Next.js tutorial</a></p>
+        <p>10 things That require zero talent.</p>
+        <p></p>
+        <ul>
+        1. Being On Time
+        2. Making An Effort
+        3. Being High Energy
+        4. Having A Positive Attitude
+        5. Being Passionate
+        6. Using Good Body Language
+        7. Being Coachable
+        8. Doing A Little Extra
+        9. Being Prepared
+        10. Having A Strong Work Ethic
+        </ul>
+        <p></p>
+        <p>(This is a sample website - you’ll be building a site like this on <a href = "https://nextjs.org/learn"> our Next.js tutorial)</a></p>
         <p></p>
       </section>
     </Layout>
